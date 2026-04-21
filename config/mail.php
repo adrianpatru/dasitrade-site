aab9f8d787e795e490ac3e8e854f669513bff475
adrianpatru <adrian.patru234@gmail.com>
2026-04-21 16:23:33 +0300

    Enable env-based SMTP password loading

--- config/mail.php
+++ config/mail.php
@@ -1,6 +1,17 @@
 <?php
 declare(strict_types=1);
 
+$smtpPassword = getenv('DASITRADE_SMTP_PASSWORD');
+if ($smtpPassword === false || $smtpPassword === '') {
+    $smtpPassword = isset($_SERVER['DASITRADE_SMTP_PASSWORD']) && is_string($_SERVER['DASITRADE_SMTP_PASSWORD'])
+        ? $_SERVER['DASITRADE_SMTP_PASSWORD']
+        : '';
+}
+
+if ($smtpPassword === '' && isset($_ENV['DASITRADE_SMTP_PASSWORD']) && is_string($_ENV['DASITRADE_SMTP_PASSWORD'])) {
+    $smtpPassword = $_ENV['DASITRADE_SMTP_PASSWORD'];
+}
+
 return [
     'transport' => 'smtp',
     'fallback_to_mail' => true,
@@ -16,7 +27,7 @@ return [
         'port' => 587,
         'security' => 'tls',
         'username' => 'smtp@dasitrade.ro',
-        'password' => '',
+        'password' => $smtpPassword,
         'auth' => true,
         'timeout' => 15,
     ],
